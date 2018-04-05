df0c0dc5d50bc4293b7db44bfd7aad5d2aadce14
majo-z <g00340481@gmit.ie>
2018-04-05 19:14:57 +0100

    added specials and stacks

--- shuntingYard.go
+++ shuntingYard.go
@@ -3,10 +3,22 @@ package main
 
 import "fmt"
 
-//inToPost takes single argument as a string and returns string
-func inToPost(infix string) string{
-pofix := ""
-	return pofix
+//inToPost converts infix regex to postfix regex
+func inToPost(infix string) string {
+
+	//map special characters into integers - order of precedence: *.|
+	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
+
+	//rune = character as displayed on screen (in UTF8)
+	//pofix := []rune{}
+
+	//stack used to temporarilly store operators as read from infix string
+	//stack := []rune{}
+
+	//shorthand
+	pofix, stack := []rune{}, []rune{}
+
+	return string(pofix) //cast/convert pofix(runes) to return back a string
 }
 
 func main() {
@@ -17,7 +29,7 @@ func main() {
 
 	//Answer: abd|.*
 	fmt.Println("Infix:   ", "(a.(b|d)*")
-	fmt.Println("Postfix: ", inToPost("(a.(b|d)*"))//postfix omits brackets
+	fmt.Println("Postfix: ", inToPost("(a.(b|d)*")) //postfix omits brackets
 	fmt.Println()
 
 	//Answer: abd|.c*

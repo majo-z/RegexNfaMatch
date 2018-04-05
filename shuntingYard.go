a95c0d553ac66ac431e554c20a745ae28a223fba
majo-z <g00340481@gmit.ie>
2018-04-06 00:46:44 +0100

    added logic to convert infix notation to postfix notation

--- shuntingYard.go
+++ shuntingYard.go
@@ -9,14 +9,46 @@ func inToPost(infix string) string {
 	//map special characters into integers - order of precedence: *.|
 	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
 
-	//rune = character as displayed on screen (in UTF8)
-	//pofix := []rune{}
-
-	//stack used to temporarilly store operators as read from infix string
-	//stack := []rune{}
-
-	//shorthand
+	//stack used to temporarily store operators as read from infix string
 	pofix, stack := []rune{}, []rune{}
+
+	//loop over the infix(input) string to convert it to postfix string
+	for _, r := range infix { //r is char, range converts the array of strings to runes
+
+		switch {
+
+		case r == '(':
+			stack = append(stack, r) //temporarily append bracket at the end of stack
+
+		case r == ')':
+
+			//loop over the stack until open bracket is found
+			for stack[len(stack)-1] != '(' {
+
+				pofix = append(pofix, stack[len(stack)-1]) //pop the chars off the stack and append on to postfix(output)
+				stack = stack[:len(stack)-1] // colon means up to last character
+			}
+			stack = stack[:len(stack)-1] //pop bracket "(" off the end of the stack
+
+		case specials[r] > 0: //special characters...0 value returned back if special character not in the map
+
+			//while the stack still has elements and precedence of the current character <= to the precedence of the top element of the stack
+			for len(stack) > 0 && specials[r] <= specials[stack[len(stack)-1]] {
+				pofix, stack = append(pofix, stack[len(stack)-1]), stack[:len(stack)-1]
+			}
+			stack = append(stack, r)//element of the stack has less precedence
+
+		default: //r is neither a bracket nor a special character(may be a, b, c, d...)
+			pofix = append(pofix, r)
+
+		} //switch
+
+	} //for
+
+	//if there is anything on the stack
+	for len(stack) > 0 {
+		pofix, stack = append(pofix, stack[len(stack)-1]), stack[:len(stack)-1]
+	}
 
 	return string(pofix) //cast/convert pofix(runes) to return back a string
 }
@@ -28,13 +60,13 @@ func main() {
 	fmt.Println()
 
 	//Answer: abd|.*
-	fmt.Println("Infix:   ", "(a.(b|d)*")
-	fmt.Println("Postfix: ", inToPost("(a.(b|d)*")) //postfix omits brackets
+	fmt.Println("Infix:   ", "(a.(b|d))*")
+	fmt.Println("Postfix: ", inToPost("(a.(b|d))*")) //postfix omits brackets
 	fmt.Println()
 
 	//Answer: abd|.c*
-	fmt.Println("Infix:   ", "(a.(b|d).c*")
-	fmt.Println("Postfix: ", inToPost("(a.(b|d).c*"))
+	fmt.Println("Infix:   ", "a.(b|d).c*")
+	fmt.Println("Postfix: ", inToPost("a.(b|d).c*"))
 	fmt.Println()
 
 	//Answer: abb.+c.

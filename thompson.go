4e23d6834dd2eb9a1010db5c7655f4d9c8c8c2a3
majo-z <g00340481@gmit.ie>
2018-04-06 01:30:31 +0100

    added skeleton, created structs for edges and states, added function base that will be used for creating nfa from regular expressions.

--- thompson.go
+++ thompson.go
@@ -1,1 +1,31 @@
+//Thomson's algorithm(construction) builds NFA from regular expression (regex is in postfix notation)
 package main
+
+import "fmt"
+
+type state struct { //struct to store states and arrows
+	symbol rune //default is 0, represents a state that has 2 arrows coming from it
+	//pointers to other states - max number of arrows coming from any state is 2
+	edge1 *state
+	edge2 *state
+}
+
+//helper struct, keeps track of initial state and accept state of fragment of nfa
+type nfa struct {
+	initial *state
+	accept  *state
+}
+
+//postfix regular expression to nfa
+func postRegToNfa(pofix string) *nfa {
+	nfaStack := []*nfa{} //pointers to nfa's
+ 
+
+	return nfaStack[0] //returns nfa struct addresses
+	
+}
+
+func main() {
+	nfa := postRegToNfa("ab.c*|") //postfix notation
+	fmt.Println(nfa)
+}

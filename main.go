19b4659c1f9827a94fe57bdfb54f514e9289ed86
majo-z <g00340481@gmit.ie>
2018-04-07 15:18:29 +0100

    added menu with different options

--- main.go
+++ main.go
@@ -8,10 +8,57 @@ package main
 import (
 	"fmt"
 	"./algorithm"
+	"bufio"
+	"os"
 )
+// global scanner
+var scanner *bufio.Scanner
 
 func main() {
+	scanner = bufio.NewScanner(os.Stdin)
 
+	for { //while
+		fmt.Printf("1. Enter your own experssion, 2. Print examples, 3. Exit:")
+		scanner.Scan()
+		userChoice := scanner.Text()
+
+		switch userChoice {
+
+		case "1":
+			readInput()
+		case "2":
+			printExamples()
+		case "3":
+			fmt.Println("Goodbye")
+			return//return back to menu option
+		default:
+			fmt.Println("Incorrect option, please enter 1, 2 or 3")
+		}
+	}
+
+}//main
+
+func readInput() {
+	fmt.Println("Enter infix regex:")
+	scanner.Scan()
+	regex := scanner.Text()
+
+	fmt.Println("Enter test string:")
+	scanner.Scan()
+	testString := scanner.Text()
+
+	if algorithm.DoesMatch(regex, testString){
+		fmt.Println("the regular expression", regex, "matched the string", testString)
+
+	}else{
+		fmt.Println("the regular expression", regex, "didn't match the string", testString)
+
+	}
+	fmt.Println()
+}
+
+func printExamples() {
+	fmt.Println()
 	fmt.Println("Infix regex \t Postfix regex \t\t Test string \t Match")
 	fmt.Println("===========================================================")
 
@@ -29,7 +76,6 @@ func main() {
 	fmt.Println("a.b.c* \t\t\t", algorithm.InToPost("a.b.c*"), "\t\t\t abd \t\t\t", algorithm.DoesMatch("a.b.c*", "abd"))
 	fmt.Println()
 
-
 	//infix a.b|c*
 	fmt.Println("a.b|c* \t\t\t", algorithm.InToPost("a.b|c*"), "\t\t\t abc \t\t\t", algorithm.DoesMatch("a.b|c*", "abc"))
 
@@ -42,7 +88,6 @@ func main() {
 	fmt.Println("a.b|c* \t\t\t", algorithm.InToPost("a.b|c*"), "\t\t\t c \t\t\t\t", algorithm.DoesMatch("a.b|c*", "c"))
 	fmt.Println()
 
-
 	//infix a.(b|d).c*
 	fmt.Println("a.(b|d).c* \t\t", algorithm.InToPost("a.(b|d).c*"), "\t\t\t abc \t\t\t", algorithm.DoesMatch("a.(b|d).c*", "abc"))
 
@@ -54,6 +99,4 @@ func main() {
 
 	fmt.Println("a.(b|d).c* \t\t", algorithm.InToPost("a.(b|d).c*"), "\t\t\t abdc \t\t\t", algorithm.DoesMatch("a.(b|d).c*", "abdc"))
 	fmt.Println()
-
-
 }
